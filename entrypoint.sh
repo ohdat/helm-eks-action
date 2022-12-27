472625e2ea3413151557d1abf306130530a0cf23
li mingzhuang <limingzhuang@ohdat.io>
2022-12-27 18:50:28 +0800

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ export KUBECONFIG="${PWD}/kubeconfig"
 
 echo "running entrypoint command(s)"
 
-response=$(sh -c " $*")
+#response=$(sh -c " $*")
 #echo "::set-output name=response::$response"
-echo "response=${response}" >> "$GITHUB_OUTPUT"
+#echo "response=${response}" >> "$GITHUB_OUTPUT"
 
-#echo "response<<EOF" >> $GITHUB_OUTPUT
-#echo "$(sh -c "$*")" >> $GITHUB_OUTPUT
-#echo "EOF" >> $GITHUB_OUTPUT
+echo "response<<EOF" >> $GITHUB_OUTPUT
+echo "$(sh -c "$*")" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
